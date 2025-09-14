00bf7af8ba4d8ed01450e4910f6d2123a4688d87
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-09-14 17:33:59 +0100

    Phase 2: Theme Customization,Font , Date fns packages done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from "../assets/logo.png";
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3'>
             <img src={logo} alt="" />
             <p>News Will be Fear and Without Fear</p>
+            <p>{format(new Date(),"EEEE,MMMM dd,yyyy")}</p>
         </div>
     );
 };
