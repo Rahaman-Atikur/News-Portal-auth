5059ba98cdff36b1028e0fec5a57d8b52fb26d18
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-09-17 23:04:14 +0100

    Navbar is working

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,17 @@
 import React, { use } from 'react';
+import { NavLink } from 'react-router';
 const categoryPromise = fetch("/categories.json").then((res) => res.json())
 const Categories = () => {
     const categories = use(categoryPromise);
-    console.log(categories);
+    // console.log(categories);
     return (
         <div>
             <h2 className='font-bold'>All Categories</h2>
+            <div className='gird grid-cols-1 mt-5 gap-3'>
+                {
+                    categories.map((category) => <NavLink className="btn bg-base-100 border-0 hover:bg-base-200" key={category.id}>{category.name}</NavLink>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/HomeLayout/LeftAside.jsx
+++ src/components/HomeLayout/LeftAside.jsx
@@ -1,16 +1,12 @@
 import React, { Suspense } from 'react';
 import Categories from '../Categories';
-
-
 const LeftAside = () => {
     return (
         <div>
             <Suspense fallback={"Is Loading so wait"}>
                 <Categories></Categories>
             </Suspense>
-
         </div>
     );
-};
-
+}
 export default LeftAside;
