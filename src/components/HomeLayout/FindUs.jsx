38640bd434d85fc5fe0189dc6bbc4c549e2b562b
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-09-19 22:40:47 +0100

    Home M51-V10 Done

--- src/components/HomeLayout/FindUs.jsx
+++ src/components/HomeLayout/FindUs.jsx
@@ -6,7 +6,7 @@ const FindUs = () => {
     return (
         <div>
             <h2 className='font-bold mb-5'>Find Us </h2>
-            <div className='join join-vertical gap-5'>
+            <div className='join join-vertical gap-5 w-full'>
                 <button className="btn justify-start   btn-secondary"> <FaXTwitter /> Twitter</button>
                 <button className="btn justify-start  btn-secondary"> <FaFacebook size={24} />  Facebook</button>
                 <button className="btn justify-start  btn-secondary"> <FaInstagram size={24} /> Instagram</button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Navigate } from 'react-router';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is Home Pages</h2>
+            <Navigate to="/category/1"></Navigate>
         </div>
     );
 };
