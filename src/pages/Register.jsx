7f7e2be0f5ebdc2f7432fc7bea3da3377aecfc2f
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-09-21 22:43:20 +0100

    Register Added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            Register
+
+            <div className='flex justify-center items-center '>
+
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl ">
+                    <h2 className='mx-auto font-extrabold text-2xl'>Register Your Account</h2>
+                    <div className="card-body">
+
+                        <fieldset className="fieldset">
+                            <label className="label">Name</label>
+                            <input type="text" className="input" placeholder="Name" />
+                            <label className="label">Email</label>
+                            <input type="email" className="input" placeholder="Email" />
+                            <label className="label">Password</label>
+                            <input type="password" className="input" placeholder="Password" />
+                            <div><a className="link link-hover">Forgot password?</a></div>
+                            <button className="btn btn-neutral mt-4">Register</button>
+                        </fieldset>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
