abcb6040eb0efa77849c86ed4f98444904ef8833
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-09-23 22:32:15 +0100

    all input is recieved

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useState } from 'react';
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
-    const [user,setUser] = useState({name:"Atikur",email:"atikur.uk.rahaman@gmail.com"});
+    const [user,setUser] = useState(null);
     const authData = {user: user,setUser,};
     return <AuthContext value={authData}>{children}</AuthContext>
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router';
 const Register = () => {
+    const handleRegister = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const photoUrl = form.photoUrl.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(name, photoUrl, email, password);
+
+
+
+    }
     return (
         <div>
             <div className='flex justify-center items-center'>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl ">
                     <h2 className='mx-auto font-extrabold text-2xl'>Register Your Account</h2>
-                    <div className="card-body">
+                    <form onSubmit={handleRegister} className="card-body">
                         <fieldset className="fieldset">
                             <label className="label">Name</label>
-                            <input type="text" className="input" placeholder="Name" />
+                            <input name='name' type="text" className="input" placeholder="Name" />
                             <label className="label">Photo URL</label>
-                            <input type="text" className="input" placeholder="Photo Url" />
+                            <input name='photoUrl' type="text" className="input" placeholder="Photo Url" />
                             <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
+                            <input name='email' type="email" className="input" placeholder="Email" />
                             <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
+                            <input name='password' type="password" className="input" placeholder="Password" />
                             <div><a className="link link-hover">Already Have an Account?</a>
-                            <Link className='text-blue font-bold ml-3' to ="/auth/login">Login</Link>
-                            
+                                <Link className='text-blue font-bold ml-3' to="/auth/login">Login</Link>
+
                             </div>
-                            <button className="btn btn-neutral mt-4">Register</button>
+                            <button type='submit' className="btn btn-neutral mt-4">Register</button>
                         </fieldset>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
